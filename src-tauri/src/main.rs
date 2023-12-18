191266f2fd15ad41365ba05f05d5de96437fe44d
Pagnany <eric.schmale@gmx.de>
2023-12-18 15:06:24 +0100

    get paths in rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@
 
 fn main() {
     tauri::Builder::default()
+        //.setup(setup_handler)
         .invoke_handler(tauri::generate_handler![example_feed])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
@@ -22,4 +23,148 @@ async fn example_feed() -> String {
         }
     }
     my_string
+}
+
+fn setup_handler(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let app_handle = app.handle();
+
+    println!(
+        "{}",
+        app_handle
+            .path_resolver()
+            .resource_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        app_handle
+            .path_resolver()
+            .app_config_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        app_handle
+            .path_resolver()
+            .app_data_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        app_handle
+            .path_resolver()
+            .app_local_data_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        app_handle
+            .path_resolver()
+            .app_cache_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        app_handle
+            .path_resolver()
+            .app_log_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::data_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::local_data_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::cache_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::config_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::executable_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::public_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::runtime_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::template_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::font_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::home_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::audio_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::desktop_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::document_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::download_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    println!(
+        "{}",
+        tauri::api::path::picture_dir()
+            .unwrap_or(std::path::PathBuf::new())
+            .to_string_lossy()
+    );
+    Ok(())
 }
